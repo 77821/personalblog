c9a8ed09ba170043768472e9835a7bb2b7c968fd
jianweiqi <jwq5837@163.com>
2026-04-01 18:11:02 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import MenuIndex from './components/menu/menuIndex.vue'
+</script>
+
+<template>
+  <header>
+    <MenuIndex />
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
